2ab162489b9c03dcd906f053b72a63fec821fe22
Ricardo Gava <Ricardoagava@gmail.com>
2021-05-12 22:55:40 -0300

    animate the first item of department with accordion

--- assets/js/medicenter.js
+++ assets/js/medicenter.js
@@ -1,16 +1,22 @@
+// MENU MOBILE
 const menuMobile = document.querySelector(".mobile-box");
+const menuMobileContent = document.querySelector(".menu-mobile-content")
+
+const showMenu = () => menuMobileContent.classList.toggle('animation')
 
 menuMobile.addEventListener('click', function showMenu() {
     document.querySelector(".menu-mobile-content").classList.toggle('animation');
 })
 
 
-const departmentWidget = document.querySelector(".departments-widget");
+// DEPARTMENT ACCORDION
+const departmentWidget = document.querySelector(".departments-widget")
+const widgetDetails = document.querySelector(".departments-widget-details")
+const departmentContent = document.querySelector('.departments-widget-content')
 
 departmentWidget.addEventListener('click', function accordion() {
-    const widgetDetails = document.querySelector(".departments-widget-details");
-    const departmentContent = document.querySelector('.departments-widget-content')
-    widgetDetails.classList.toggle('hidden');
+
+    widgetDetails.classList.toggle('hidden')
     departmentContent.classList.toggle('hidden');
 })
 
